4066b3bde0a2d066dd5ced962f29667232696170
James Tomasino <james@tomasino.org>
2019-06-25 14:34:24 -0400

    mini adventure for testing added

--- messages.py
+++ messages.py
@@ -5,14 +5,40 @@ error = 'Non capisco, Pappà. '
 class Messages():
     def __init__(self):
         self.state = {
-            'english': False,
-            'blue_key': False
+            'room1': True,
+            'room2': False,
+            'room3': False,
+            'blue_key': False,
+            'chest_open': False,
+            'ring': False
         }
 
         self.responses = [
-            Response('test', 'Conosci mio Pappà?'),
-            Response('monkey', 'Doom', checks=['english'], set_on=['blue_key']),
-            Response('english', 'I... think I remember that', set_on=['english'])
+            # Key
+            Response('.', 'You can\'t seem to take your eyes off the ring. It\'s so pretty.', checks=['ring']),
+
+            # Help
+            Response('help', 'Try looking around.', checks=['room1']),
+            Response('help', 'How did you get here without learning to look around?', exceptions=['room1']),
+
+            # Room 1
+            Response('look', 'You are in a dusty cave. There is a door to the north and a door to the west.', checks=['room1']),
+            Response('north', 'You move north through the doorway. Things don\'t smell so good.', checks=['room1'], set_on=['room2'], set_off=['room1']),
+            Response('west', 'You go west, my friend.', checks=['room1'], set_on=['room3'], set_off=['room1']),
+
+            # Room 2 (north)
+            Response('look', 'This room is filled with piles of trash. Amongst the trash is a lovely blue key.', checks=['room2'], exceptions=['blue_key']),
+            Response('look', 'This room is filled with piles of trash.', checks=['room2','blue_key']),
+            Response('back', 'You return the way you came. That was a gross place.', checks=['room2'], set_on=['room1'], set_off=['room2']),
+            Response('south', 'Down we go...', checks=['room2'], set_on=['room1'], set_off=['room2']),
+            Response('key|blue', 'You snatch up the blue key. It\'s pretty!', checks=['room2'], set_on=['blue_key']),
+
+            # Room 3 (west)
+            Response('look', 'A small chest is sitting in the middle of this empty room. There is a lock on it. The only exit is the way you entered.', checks=['room3'], exceptions=['chest_open']),
+            Response('look', 'An open chest is sitting in the middle of this empty room. The door to the east returns the way you came.', checks=['room3','chest_open'], set_on=['ring']),
+            Response('back', 'You return the way you came. That room was creepy anyway.', checks=['room3'], set_on=['room1'], set_off=['room3']),
+            Response('east', 'You walk through the eastern door.', checks=['room3'], set_on=['room1'], set_off=['room3']),
+            Response('key|lock|blue', 'You open the chest to reveal a beautiful golden ring. Plucking it from the velvety interior, you slip in onto your finger. It feels cool and heavy, and you have the sense that this was a mistake.', checks=['room3', 'blue_key'], set_on=['ring'])
         ]
 
     def check(self, msg_byte_arr):
@@ -24,9 +50,11 @@ class Messages():
         response = ''
         for r in self.responses:
             # check if regex matches passed string
-            if r.regex.match(msg):
+            if r.regex.search(msg):
                 # check that all required checks for response are valid too
                 if not self.test_conditions(r.checks):
+                    continue
+                if not self.test_exceptions(r.exceptions):
                     continue
                 for on in r.set_on:
                     self.state[on] = True
@@ -42,5 +70,11 @@ class Messages():
     def test_conditions(self, checks):
         for c in checks:
             if not self.state[c]:
+                return False
+        return True
+
+    def test_exceptions(self, checks):
+        for c in checks:
+            if self.state[c]:
                 return False
         return True
--- response.py
+++ response.py
@@ -8,9 +8,10 @@ import re
     response string if matched
 """
 class Response():
-    def __init__(self, match_str, resp, checks = [], set_on = [], set_off = []):
+    def __init__(self, match_str, resp, checks = [], exceptions = [], set_on = [], set_off = []):
         self.regex = re.compile(match_str)
         self.resp = resp
         self.checks = checks
+        self.exceptions = exceptions
         self.set_on = set_on
         self.set_off = set_off
