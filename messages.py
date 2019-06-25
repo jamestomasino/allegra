ddce3063fd0a049ba23442aaf152f525bf91f2e7
James Tomasino <james@tomasino.org>
2019-06-25 13:50:58 -0400

    adding state system to response object interactions to allow for basic branching

--- messages.py
+++ messages.py
@@ -4,9 +4,16 @@ error = 'Non capisco, Pappà. '
 
 class Messages():
     def __init__(self):
+        self.state = {
+            'english': False,
+            'blue_key': False
+        }
+
         self.responses = [
             Response('test', 'Conosci mio Pappà?'),
-            Response('english', 'I... think I remember that')
+            Response('monkey', 'Doom', checks=['english'], set_on=['blue_key']),
+            Response('english', 'I... think I remember that', set_on=['english'])
+
         ]
 
     def check(self, msg_byte_arr):
@@ -17,7 +24,16 @@ class Messages():
 
         response = ''
         for r in self.responses:
+            # check if regex matches passed string
             if r.regex.match(msg):
+                # check that all required checks for response are valid too
+                for c in r.checks:
+                    if not self.state[c]:
+                        continue
+                for on in r.set_on:
+                    self.state[on] = True
+                for off in r.set_off:
+                    self.state[off] = False
                 response = r.resp + '\n'
                 break
 
--- response.py
+++ response.py
@@ -1,6 +1,16 @@
 import re
 
+""" Response object
+    regex to match string input
+    array of boolean state checks for test
+    array of state toggles to true
+    array of state toggles to false
+    response string if matched
+"""
 class Response():
-    def __init__(self, match_str, resp):
+    def __init__(self, match_str, resp, checks = [], set_on = [], set_off = []):
         self.regex = re.compile(match_str)
         self.resp = resp
+        self.checks = checks
+        self.set_on = set_on
+        self.set_off = set_off
